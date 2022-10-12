3d467dda51917a2b1539a0196ba47271defc5fd4
fperazzo <fernando_perazzo@yahoo.com.br>
2022-10-12 08:51:56 -0300

    index

---
+++ core/urls.py
@@ -0,0 +1,15 @@
+from core.views import IndexTemplateView
+from django.urls import path
+from . import views
+
+app_name = 'core'
+
+urlpatterns = [
+
+
+    path('', IndexTemplateView.as_view(), name="index"),
+   #  path('pt1/', views.pt1_new, name='pt1'),
+
+
+
+ ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+class IndexTemplateView(TemplateView):
+    template_name = "core/index.html"
